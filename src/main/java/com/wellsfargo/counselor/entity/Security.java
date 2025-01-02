4b1d5619495cc4dc117ab4549078453e65e83f10
Lalith Cheruku <clalithadithya@gmail.com>
2025-01-01 18:40:00 -0700

    Create Security.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,54 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Date;
+
+@Entity
+public class Security {
+    
+    @Id
+    @GeneratedValue
+    private long securityId;
+  
+    @ManyToOne
+    @JoinColumn(name = "clientID")
+    private ClientPortfolio portfolio; // clientPortfolio referenced as a foreign key
+
+    @ManyToOne
+    @JoinColumn(name = "advisorID")
+    private Advisor advisor; // Advisor referenced as a foreign key
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+    @Column(nullable = false)
+    private Date purchaseDate;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private String category;
+
+    protected Security() {
+
+    }
+
+    public Security(long securityId, Portfolio portfolio, Advisor advisor, String name, String category, double purchasePrice, Date purchaseDate, int quantity) {
+        this.securityId = securityId;
+        this.portfolio = portfolio;
+        this.advisor = advisor;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+    }
+  }
